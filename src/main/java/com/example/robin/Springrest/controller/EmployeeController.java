1deb27424286c0b4c2eb5d861fec71cb97775fd4
Robin <robin.svensson06@gmail.com>
2020-04-16 13:43:05 +0200

    controller functions updated

--- src/main/java/com/example/robin/Springrest/controller/EmployeeController.java
+++ src/main/java/com/example/robin/Springrest/controller/EmployeeController.java
@@ -1,10 +1,44 @@
 package com.example.robin.Springrest.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.robin.Springrest.exception.ResourceNotFoundException;
+import com.example.robin.Springrest.model.Employee;
+import com.example.robin.Springrest.repository.EmployeeRepository;
+
 @RestController
+@RequestMapping("/api/v1")
 public class EmployeeController {
 	
+	@Autowired
+	private EmployeeRepository employeerepository;
 	
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeerepository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@Valid @RequestBody Employee employee) {
+		return employeerepository.save(employee);
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id")long employeeId) throws ResourceNotFoundException{
+		Employee employee = employeerepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found" + employeeId));
+		return ResponseEntity.ok().body(employee);
+	}
 
 }
